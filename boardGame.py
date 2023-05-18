d1497296e20cf772ee07620148c189bbf0a363a0
SolomiaSydoryk <sydoryk213@gmail.com>
2023-05-18 14:34:06 +0300

    added change after review

--- boardGame.py
+++ boardGame.py
@@ -1,49 +1,37 @@
 class BoardGame:
-    instance = 0
+    __instance = None
 
     def __init__(self, tittle="Any", min_players=2, max_players=4, current_players=3):
+        """
+        Initializing a BoardGame instance.
+
+        Args:
+            tittle (str): the title of the BoardGame.
+            min_players (int): the minimal players in the BoardGame.
+            max_players (int): the maximal players in the BoardGame.
+            current_players (int): the current players in BoardGame.
+        """
         self.tittle = tittle
         self.min_players = min_players
         self.max_players = max_players
         self.current_players = current_players
 
-    def get_tittle(self):
-        return self.tittle
-
-    def get_min_players(self):
-        return self.min_players
-
-    def get_max_players(self):
-        return self.max_players
-
-    def get_current_players(self):
-        return self.current_players
-
-    def set_tittle(self, t):
-        self.tittle = t
-
-    def set_min_players(self, mn):
-        self.min_players = mn
-
-    def set_max_players(self, mx):
-        self.max_players = mx
-
-    def set_current_players(self, c):
-        self.current_players = c
-
     def add_player(self):
+        """Increasing the current_players to +1. Else - setting the current_players to 0."""
         if self.current_players < self.max_players:
             return self.current_players + 1
         else:
             return 0
 
     def remove_player(self):
+        """Decreasing the current_players to -1. Else - setting the current_players to 0."""
         if self.current_players > self.min_players:
             return self.current_players - 1
         else:
             return 0
 
     def can_play(self):
+        """Decreasing current_players to -1. Else - setting current_players to 0."""
         if self.min_players <= self.current_players <= self.max_players:
             return True
         else:
@@ -51,16 +39,22 @@ class BoardGame:
 
     @staticmethod
     def get_instance():
-        if not BoardGame.instance:
-            BoardGame.instance = BoardGame()
-        return BoardGame.instance
+        """
+        Getting an instance of the BoardGame class.
+        Returns:
+        BoardGame: The singleton instance of the BoardGame class.
+        """
+        if not BoardGame.__instance:
+            BoardGame.__instance = BoardGame()
+        return BoardGame.__instance
 
     def __str__(self):
-        return "BoardGame(tittle={}, min_players={}, max_players={}, current_players={})"\
+        """Returning a string representation of BoardGame class."""
+        return "BoardGame(tittle={}, min_players={}, max_players={}, current_players={})" \
             .format(self.tittle, self.min_players, self.max_players, self.current_players)
 
 
-games = [BoardGame(), BoardGame("Monopoly", 2, 8), BoardGame.get_instance(), BoardGame.get_instance()]
-
-for game in games:
-    print(game)
+if __name__ == "__main__":
+    games = [BoardGame(), BoardGame("Monopoly", 2, 8), BoardGame.get_instance(), BoardGame.get_instance()]
+    for game in games:
+        print(game)
