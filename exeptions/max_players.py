05b5ad478064fbcb6d99a40ef4de53ab186f4256
SolomiaSydoryk <119521489+SolomiaSydoryk@users.noreply.github.com>
2023-06-21 12:44:17 +0300

    Update max_players.py
    updated version of the code before handing it over

--- exeptions/max_players.py
+++ exeptions/max_players.py
@@ -4,15 +4,18 @@ from functools import wraps
 
 
 class TooManyPlayersException(Exception):
-    pass
+    """Class for my exceptions"""
+
+    def __str__(self):
+        return "Maximum number of players already reached."
 
 
-def logged(exception, mode):
+def logged(exception_type, mode):
     """
        A decorator that logs exceptions raised in a function to a file or console.
 
        Args:
-           exception (Exception): The exception to catch.
+           exception_type (Exception): The exception to catch.
            mode (str): The mode to log the exceptions. Can be `file` or `console`.
        """
 
@@ -21,15 +24,19 @@ def logged(exception, mode):
         def wrapper(*args, **kwargs):
             try:
                 return func(*args, **kwargs)
-            except exception as e:
+            except exception_type as exc:
                 class_name = args[0].__class__.__name__
-                message = f"Exception {exception.__name__} was raised in {class_name}.{func.__name__} with message: {e}"
+                message = f"Exception {exception_type.__name__} was raised in {class_name}.{func.__name__} with " \
+                          f" message: {exc}"
                 if mode == "file":
-                    logging.basicConfig(filename='exceptions.log', level=logging.ERROR)
-                    logging.error(message)
+                    logging.basicConfig(filename='exceptions.log', level=logging.WARNING, filemode="w")
+                    logging.warning(message)
                 elif mode == "console":
-                    logging.basicConfig(level=logging.ERROR)
-                    logging.error(message)
+                    logging.basicConfig(level=logging.WARNING)
+                    logging.warning(message)
+                else:
+                    raise ValueError("You are invalid") from exc
+                return TooManyPlayersException
 
         return wrapper
 
